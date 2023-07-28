b8b7aefc7afdbd1459d9e74f81a497d73a0414c8
BenceSzalai <bence@sbnc.eu>
2023-07-28 02:37:32 +0200

    build: update typescript to v4.9, also ts-jest, jest and related dependencies to compatible versions

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testMatch: ['<rootDir>/packages/*/test/**/*.spec.js'],
   setupFiles: ['./test/__helpers__/setup.js'],
-  setupFilesAfterEnv: ['jest-extended'],
+  setupFilesAfterEnv: ['jest-extended/all'],
   testEnvironment: 'node',
   preset: 'ts-jest/presets/default',
   collectCoverageFrom: ['packages/*/src/**/*.ts']
